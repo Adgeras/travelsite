e3da71b52a5ffd80f4dbbc7c8fe7de1875ce8c44
Edgariukstis <edgaras.banaitis@gmail.com>
2020-09-12 11:05:29 +0300

    created mrc

---
+++ app/Http/Controllers/TownsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Towns;
+use Illuminate\Http\Request;
+
+class TownsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Towns  $towns
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Towns $towns)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Towns  $towns
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Towns $towns)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Towns  $towns
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Towns $towns)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Towns  $towns
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Towns $towns)
+    {
+        //
+    }
+}
